ce72f5eede48990b390071a1aa21856939c13d91
FlorangelValverde <florangelvalverde@upeu.edu.pe>
2021-05-11 09:07:32 -0500

    ultimo intento

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+$database =  'ventas';
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$conn = mysqli_connect($servername, $username, $password, $database);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}else{
+echo("Conexión exitosa");
+}
+?>
